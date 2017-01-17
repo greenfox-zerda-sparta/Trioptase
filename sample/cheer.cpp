7ff5ff6f5fa963192a80f20a7fb4f29f4f70bb45
tataiferenc89 <tatai.ferenc89@gmail.com>
2017-01-17 10:44:47 +0100

    c1

---
+++ sample/cheer.cpp
@@ -0,0 +1,19 @@
+//============================================================================
+// Name        : cheer.cpp
+// Author      : Feri
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+void cheer() {
+  cout << "Trioptase" << endl;
+}
+
+int main() {
+
+	return 0;
+}

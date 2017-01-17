a546efa29369a22a38d6707a86c49791e9d2d2c0
Czetenyi Kristof <wekkew@gmail.com>
2017-01-17 14:26:15 +0100

    SDL - draw picture without any render

---
+++ SDL_draw/SDL_draw/SDL_draw/Source.cpp
@@ -0,0 +1,38 @@
+#include <SDL.h>
+#include <iostream>
+
+using std::cerr;
+using std::endl;
+
+const int WINDOW_HEIGHT = 320;
+const int WINDOW_WIDTH = 320;
+
+int main(int argc, char* argv[]) {
+  SDL_Init(SDL_INIT_EVERYTHING);
+  SDL_Window* window;
+  SDL_Surface* pureSurface;
+  SDL_Surface* picture;
+  SDL_Surface* picture2;
+  
+  window = SDL_CreateWindow("title", 0, 0, WINDOW_WIDTH, WINDOW_HEIGHT, 0);
+  // get the empty window as a surface
+  pureSurface = SDL_GetWindowSurface(window);
+ /// pureSurface = SDL_LoadBMP("lego.bmp");
+  picture = SDL_LoadBMP("pic.bmp");
+  picture2 = SDL_LoadBMP("lego.bmp");
+ 
+  // copy picture surface to the pureSurface (which is the empty window)
+  SDL_BlitSurface(picture2, NULL, pureSurface, NULL);
+  SDL_BlitSurface(picture, NULL, pureSurface, NULL);
+  
+
+  SDL_FreeSurface(picture);
+  SDL_UpdateWindowSurface(window);
+      
+  SDL_Delay(2000);
+  
+  SDL_DestroyWindow(window);
+  SDL_QUIT;
+
+  return 0;
+}

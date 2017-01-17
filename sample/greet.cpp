74925bf1cc0d361d6a5227ef61670f092aebdac1
tataiferenc89 <tatai.ferenc89@gmail.com>
2017-01-17 10:51:09 +0100

    c3

--- sample/greet.cpp
+++ sample/greet.cpp
@@ -10,6 +10,7 @@ int main(int argc, char* argv[]) {
 }
 
 void printname(char* name) {
+  cout << "Greetings" << endl;
   cout << "My name is: " << name;
   cout << " and I was born in " << 5 << ". month";
 }

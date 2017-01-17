697ff497576c225b104c19db171d6d1342b04432
Czetenyi Kristof <wekkew@gmail.com>
2017-01-17 11:18:04 +0100

    b3

--- sample/cheer.cpp
+++ sample/cheer.cpp
@@ -7,14 +7,17 @@
 //============================================================================
 
 #include <iostream>
+
 using namespace std;
 
-void cheer(int number) {
+void cheer(int number, char* baseLanguage) {
   cout << "Trioptase " << number << endl;
+  cout << "my chosen base language is: " << baseLanguage << endl; 
 }
 
 int main() {
+  char* lang = "c++";
   int number_of_team_members = 3;
-  cheer(number_of_team_members);
-	return 0;
+  cheer(number_of_team_members, lang);
+  return 0;
 }

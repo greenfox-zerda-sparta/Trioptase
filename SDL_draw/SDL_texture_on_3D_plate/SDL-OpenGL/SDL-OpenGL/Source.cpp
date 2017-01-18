fa9e81bb43793e8bfda16a20f916b275e59a7f6e
Czetenyi Kristof <wekkew@gmail.com>
2017-01-18 15:52:07 +0100

    GL window kristof style : - #sparta green - window header in view

---
+++ SDL_draw/SDL_texture_on_3D_plate/SDL-OpenGL/SDL-OpenGL/Source.cpp
@@ -0,0 +1,23 @@
+#include <SDL.h>
+#include <SDL_opengl.h>
+#include <iostream>
+
+float rgbToGl(float);
+
+int main(int argc, char* argv[]) {
+  SDL_Window* window = SDL_CreateWindow("SDL2/OpenGL Demo", 0, 30, 640, 480, SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE);
+  SDL_GLContext glcontext = SDL_GL_CreateContext(window);
+
+  glClearColor(rgbToGl(89), rgbToGl(162), rgbToGl(122), 1);
+  glClear(GL_COLOR_BUFFER_BIT);
+  SDL_GL_SwapWindow(window);
+  SDL_Delay(5000);
+  SDL_GL_DeleteContext(glcontext);
+  SDL_DestroyWindow(window);
+
+  return 0;
+}
+
+float rgbToGl(float rgb) {
+  return rgb/255;
+}

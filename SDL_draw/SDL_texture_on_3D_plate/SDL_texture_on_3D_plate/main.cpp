f46dbbfc96bd19cf2edc7bde80e8917fa7ccf09f
tataiferenc89 <tatai.ferenc89@gmail.com>
2017-01-18 15:24:07 +0100

    commit

--- SDL_draw/SDL_texture_on_3D_plate/SDL_texture_on_3D_plate/main.cpp
+++ SDL_draw/SDL_texture_on_3D_plate/SDL_texture_on_3D_plate/main.cpp
@@ -1,15 +1,16 @@
 #include <SDL.h>
+#include <SDL_opengl.h>
 #include <iostream>
 
 int main(int argc, char* argv[]) {
   SDL_Window *window;
-  window = SDL_CreateWindow("SDL2/OpenGL Demo", 0, 0, 640, 480, SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE);
+  window = SDL_CreateWindow("SDL2/OpenGL Demo", 50, 50, 640, 480, SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE);
   SDL_GLContext glcontext = SDL_GL_CreateContext(window);
   glClearColor(0, 0, 0, 1);
   glClear(GL_COLOR_BUFFER_BIT);
   SDL_GL_SwapWindow(window);
   SDL_Delay(5000);
-  //SDL_GLContext SDL_GL_CreateContext(SDL_Window* window);
+  SDL_GLContext SDL_GL_CreateContext(SDL_Window* window);
   SDL_GL_DeleteContext(glcontext);
   return 0;
 

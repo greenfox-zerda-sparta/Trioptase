abcfe1db1fb632ee571e496cc40cb859fdd081d5
korompaidani <korompaidani@gmail.com>
2017-01-19 15:20:00 +0100

    seconde line was created

--- sample/Branch_practice/main.cpp
+++ sample/Branch_practice/main.cpp
@@ -3,6 +3,7 @@
 using std::cout;
 
 int main(int argc, char* argv[]) {
-  cout << "this is our first branch practice project";
+  cout << "this is our first branch practice project\n";
+  cout << "second line\n";
   return 0;
 }

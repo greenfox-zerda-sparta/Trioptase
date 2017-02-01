3d8b776ff03684a9ce07b30eb4b399207d6769de
korompaidani <korompaidani@gmail.com>
2017-02-01 12:05:56 +0100

    working pathfinder header

--- SDL_draw/SDL_map_moving_width_mouse_2/Node.hpp
+++ SDL_draw/SDL_map_moving_width_mouse_2/Node.hpp
@@ -1,27 +1,8 @@
 #pragma once
-#include <iostream>
-#include <iomanip>
-#include <queue>
-#include <string>
-#include <math.h>
-#include <ctime>
-#include <vector>
-
 class Node {
 private:
-  int pos_x;
-  int pos_y;
-  int level;
-  int priority;
-  const int& estimate(const int& dest_x, const int& dest_y) const;
 public:
   Node();
-  Node(int _pos_x, int _pos_y, int _dest, int _prior);
-  int get_pos_x();
-  int get_pos_y();
-  int get_level();
-  int get_priority();
-  void update_priority(const int& dest_x, const int& dest_y);
-  void next_level(const int& i);
   ~Node();
 };
+
--- SDL_draw/SDL_map_moving_width_mouse_2/main.cpp
+++ SDL_draw/SDL_map_moving_width_mouse_2/main.cpp
@@ -4,9 +4,7 @@
 #include "User_input.hpp"
 #include "Map.hpp"
 #include "Texture_manager.hpp"
-#include "Node.hpp"
-#include "Path_finder.hpp"
-
+#include "Path_scratcher.hpp"
 
 #ifndef CATCH_CONFIG_MAIN
 
@@ -90,88 +88,16 @@ int ticker(int steps) {
 
 #ifdef CATCH_CONFIG_MAIN
 
-const int n = 60; // horizontal size of the map
-const int m = 60; // vertical size size of the map
-std::vector<std::vector<int>> tile_map;
-const int dir = 8;
-static int dx[dir] = { 1, 1, 0, -1, -1, -1, 0, 1 };
-static int dy[dir] = { 0, 1, 1, 1, 0, -1, -1, -1 };
-
 using namespace std;
 int main(int argc, char* argv[]) {
-  srand(time(NULL));
-
-  // create empty map
-  tile_map.resize(n);
-  for (int i = 0; i < tile_map.size(); i++) {
-    tile_map[i].resize(m, 0);
-  }
-
-  // fillout the map matrix with a '+' pattern
-  for (int x = n / 8; x<n * 7 / 8; x++) {
-    tile_map[x][m / 2] = 1;
-  }
-  for (int y = m / 8; y<m * 7 / 8; y++) {
-    tile_map[n / 2][y] = 1;
-  }
-
-  int xA, yA, xB, yB;
-  xA = 1;
-  yA = 1;
-  xB = 59;
-  yB = 59;
-
-  cout << "Map Size (X,Y): " << n << "," << m << endl;
-  cout << "Start: " << xA << "," << yA << endl;
-  cout << "Finish: " << xB << "," << yB << endl;
-  // get the route
-  clock_t start = clock();
-
-  Path_finder pf(tile_map);
-
-  string route = pf.find_path(xA, yA, xB, yB);
-  if (route == "") cout << "An empty route generated!" << endl;
-  clock_t end = clock();
-  double time_elapsed = double(end - start);
-  cout << "Time to calculate the route (ms): " << time_elapsed << endl;
-  cout << "Route:" << endl;
-  cout << route << endl << endl;
-
-  // follow the route on the map and display it 
-  if (route.length()>0) {
-    int j; char c;
-    int x = xA;
-    int y = yA;
-    pf.map[x][y] = 2;
-    for (int i = 0; i<route.length(); i++) {
-      c = route.at(i);
-      j = atoi(&c);
-      x = x + dx[j];
-      y = y + dy[j];
-      pf.map[x][y] = 3;
+  route_planning();
+  for (int y = 0; y < m; y++) {
+    for (int x = 0; x < n; x++) {
+      std::cout << tile_map[x][y];
     }
-    pf.map[x][y] = 4;
-
-    // display the map with the route
-    for (int y = 0; y<m; y++) {
-      for (int x = 0; x < n; x++) {
-        if (pf.map[x][y] == 0)
-          cout << ".";
-        else if (pf.map[x][y] == 1)
-          cout << "O"; //obstacle
-        else if (pf.map[x][y] == 2)
-          cout << "S"; //start
-        else if (pf.map[x][y] == 3)
-          cout << "R"; //route
-        else if (pf.map[x][y] == 4)
-          cout << "F"; //finish
-        cout << endl;
-      }
-    }
+    std::cout << std::endl;
   }
-  getchar(); // wait for a (Enter) keypress  
-  
-  return 0;    
-  }
+  return 0;
+}
 
 #endif

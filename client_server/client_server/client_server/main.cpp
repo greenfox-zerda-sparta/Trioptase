4f911db6ab4f77d8abd63f308e6fa3a6ca8652a7
korompaidani <korompaidani@gmail.com>
2017-02-14 11:40:16 +0100

    server send method test is ready to use

--- client_server/client_server/client_server/main.cpp
+++ client_server/client_server/client_server/main.cpp
@@ -7,15 +7,16 @@
 #include "Client_cl.hpp"
 
 int main(int argc, char* argv[]) {
-  //Server_sr my_server;
-  //std::cout << "Server is waiting for client" << std::endl;
-  //my_server.server_init();
+  Server_sr my_server;
+  std::cout << "Server is waiting for client" << std::endl;
+  my_server.server_init();
   //std::cout << my_server.server_receive() << std::endl;
-  //my_server.server_close();
-  Client_cl my_client;
-  my_client.client_init();
-  my_client.client_send("cicca");
-  my_client.client_close();
+  my_server.server_send("Buzz Lightyear");
+  my_server.server_close();
+  //Client_cl my_client;
+  //my_client.client_init();
+  //my_client.client_send("cicca");
+  //my_client.client_close();
 
 
   return 0;

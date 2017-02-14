f9d347c00d44fa64bb9c07e6522cd513e82a7f42
tataiferenc89 <tatai.ferenc89@gmail.com>
2017-02-14 12:31:54 +0100

    client json ready for testing

--- client_server/client_server/client_server/main.cpp
+++ client_server/client_server/client_server/main.cpp
@@ -7,18 +7,18 @@
 #include "Client_cl.hpp"
 
 int main(int argc, char* argv[]) {
-  Server_sr my_server;
-  std::cout << "Server is waiting for client" << std::endl;
-  my_server.server_init();
-  //std::cout << my_server.server_receive() << std::endl;  
-  //json jani = { {"hero", "Buzz Lightyer"}, {"numba", 5000} };
-  my_server.server_send({ { "hero", "Buzz Lightyer" },{ "numba", 5000 } });
-  my_server.server_close();
-  //Client_cl my_client;
-  //my_client.client_init();
-  //my_client.client_send("cicca");
-  //std::cout << my_client.client_receive() << std::endl;
-  //my_client.client_close();
+  //Server_sr my_server;
+  //std::cout << "Server is waiting for client" << std::endl;
+  //my_server.server_init();
+  ////std::cout << my_server.server_receive() << std::endl;  
+  ////json jani = { {"hero", "Buzz Lightyer"}, {"numba", 5000} };
+  //my_server.server_send({ { "hero", "Buzz Lightyer" },{ "numba", 5000 } });
+  //my_server.server_close();
+  Client_cl my_client;
+  my_client.client_init();
+ // my_client.client_send("cicca");
+  std::cout << my_client.client_receive() << std::endl;
+  my_client.client_close();
 
 
   return 0;

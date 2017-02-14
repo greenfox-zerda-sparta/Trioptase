25b66a1f5618b257706f1144378140968114fa91
tataiferenc89 <tatai.ferenc89@gmail.com>
2017-02-14 11:33:20 +0100

    client cleaned, basic send working

--- client_server/client_server/client_server/Client_cl.cpp
+++ client_server/client_server/client_server/Client_cl.cpp
@@ -8,54 +8,32 @@ void Client_cl::client_init() {
   SDL_Init(SDL_INIT_EVERYTHING);
   SDLNet_Init();
   set = SDLNet_AllocSocketSet(2);
-  SDLNet_ResolveHost(&ip, "127.0.0.1", 1234); //192.168.0.7  127.0.0.1
+  SDLNet_ResolveHost(&ip, "10.27.6.25", 1234); //192.168.0.7  127.0.0.1
   this->client = SDLNet_TCP_Open(&ip);
   SDLNet_TCP_AddSocket(set, client);
   this->activeSockets = SDLNet_CheckSockets(set, 10);
 }
 
-void Client_cl::client_chat() {
-  while (1) {
-    server_mess = server_chars;
-    int activeSockets = SDLNet_CheckSockets(set, 10);
-    if (activeSockets != 0) {
-      int gotMessage = SDLNet_SocketReady(client);
-      if (gotMessage != 0) {
-        SDLNet_TCP_Recv(client, server_chars, 100);
-        if (server_mess == "exit" || server_mess == "quit") {
-          break;
-        }
-        std::cout << server_mess << std::endl;
-      }
-    }
-    if (_kbhit() != 0) {
-      getline(std::cin, client_mess);
-      client_chars = client_mess.c_str();
-      SDLNet_TCP_Send(client, client_chars, client_mess.length() + 1);
-      if (client_mess == "exit" || client_mess == "quit") {
-        break;
-      }
-    }
-  }
-}
 
 void Client_cl::client_send(std::string client_mess) {
-  while (1) {
+  bool running = true;
+  while (running) {
     client_chars = client_mess.c_str();
     SDLNet_TCP_Send(client, client_chars, client_mess.length() + 1);
-    break;
+    running = false;
   }
 }
 
 std::string Client_cl::client_receive() {
-  while (1) {
+  bool running = true;
+  while (running) {
     this->activeSockets = SDLNet_CheckSockets(set, 10);
     if (this->activeSockets != 0) {
       gotMessage = SDLNet_SocketReady(client);
       if (gotMessage != 0) {  
         SDLNet_TCP_Recv(client, server_chars, 100);
         server_mess = server_chars;
-        break;
+        running = false;
       }
     }
   }
@@ -67,13 +45,6 @@ void Client_cl::client_close() {
   SDLNet_FreeSocketSet(set);
   SDL_Quit();
 }
-
-void Client_cl::client_run() {
-  this->client_init();
-  this->client_chat();
-  this->client_close();
-}
-
 
 Client_cl::~Client_cl() {
 }
--- client_server/client_server/client_server/main.cpp
+++ client_server/client_server/client_server/main.cpp
@@ -4,13 +4,19 @@
 #include <SDL.h>
 #include <SDL_net.h>
 #include "Server_sr.hpp"
+#include "Client_cl.hpp"
 
 int main(int argc, char* argv[]) {
-  Server_sr my_server;
-  std::cout << "Server is waiting for client" << std::endl;
-  my_server.server_init();
-  std::cout << my_server.server_receive() << std::endl;
-  my_server.server_close();
+  //Server_sr my_server;
+  //std::cout << "Server is waiting for client" << std::endl;
+  //my_server.server_init();
+  //std::cout << my_server.server_receive() << std::endl;
+  //my_server.server_close();
+  Client_cl my_client;
+  my_client.client_init();
+  my_client.client_send("cicca");
+  my_client.client_close();
+
 
   return 0;
 }

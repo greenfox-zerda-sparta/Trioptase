484d10d90688eaac93da77782c032bb26bea031d
tataiferenc89 <tatai.ferenc89@gmail.com>
2017-02-14 11:43:42 +0100

    Client recive is tested

--- client_server/client_server/client_server/main.cpp
+++ client_server/client_server/client_server/main.cpp
@@ -7,16 +7,17 @@
 #include "Client_cl.hpp"
 
 int main(int argc, char* argv[]) {
-  Server_sr my_server;
-  std::cout << "Server is waiting for client" << std::endl;
-  my_server.server_init();
-  //std::cout << my_server.server_receive() << std::endl;
-  my_server.server_send("Buzz Lightyear");
-  my_server.server_close();
-  //Client_cl my_client;
-  //my_client.client_init();
-  //my_client.client_send("cicca");
-  //my_client.client_close();
+  //Server_sr my_server;
+  //std::cout << "Server is waiting for client" << std::endl;
+  //my_server.server_init();
+  ////std::cout << my_server.server_receive() << std::endl;
+  //my_server.server_send("Buzz Lightyear");
+  //my_server.server_close();
+  Client_cl my_client;
+  my_client.client_init();
+  my_client.client_send("cicca");
+  std::cout << my_client.client_receive() << std::endl;
+  my_client.client_close();
 
 
   return 0;

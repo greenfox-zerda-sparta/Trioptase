80287c46fc579ec5eee9c77a3fa45468e58c9b08
korompaidani <korompaidani@gmail.com>
2017-02-15 16:26:09 +0100

    garbage collection in main by me were made

--- client_server/client_server/client_server/main.cpp
+++ client_server/client_server/client_server/main.cpp
@@ -10,40 +10,7 @@
 int main(int argc, char* argv[]) {
 
   //Server_sr my_server;
-  //json jani = { { "hero", "Buzz Lightyear" } };
-  //jani.to_msgpack;
-  //std::cout << jani << std::endl;
-  //std::cout << sizeof(jani) << std::endl;
-  //std::vector<uint8_t> danika = json::to_msgpack(jani);
-  //std::cout << danika.size() << std::endl;
-  //
-  //char text[21];
-  //for (int i = 0; i < danika.size(); i++) {
-  //  text[i] = danika[i];
-  //}
-
-  //for (int i = 0; i < danika.size(); i++) {
-  //  std::cout << text[i];
-  //}
-  //std::cout << std::endl;
-
-  //std::vector<uint8_t> ferike;
-
-  //for (int i = 0; i < danika.size(); i++) {
-  //  ferike.push_back(text[i]);
-  //}
-
-  //for (int i = 0; i < danika.size(); i++) {
-  //  std::cout << ferike[i];
-  //}
-
-
-  //json jackson = json::from_msgpack(ferike);
-
-  //std::cout << std::endl << "Jackson Five's hero is = " << jackson["hero"] << std::endl;
-
-  //std::cout << std::endl;
-  ////std::cout << jani.to_msgpack << std::endl;
+  //json jani = { { "hero", "Buzz Lightyear" } };  
   //std::cout << "Server is waiting for client" << std::endl;
   //my_server.server_init();  
   //my_server.server_send(jani);
